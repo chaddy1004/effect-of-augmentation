afeaf97d0934923a21e678a97944395408b4a95c
Chad Paik <chad.paik@voyagerx.com>
2019-11-08 11:29:45 -0500

    multigpu code

--- network.py
+++ network.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.layers import Input, Dense, Flatten, MaxPooling2D, LeakyReLU, Activation
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
 
 from layers import conv_block_2d, apply_padding
 
@@ -34,7 +35,9 @@ class VGG11():
         return Model(img, scores, name=model_name)
 
     def compile_model(self):
-        model = self.define_model("VGG11")
-        opt = Adam(lr=self.config.model.lr)
-        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
+        strategy = tf.distribute.MirroredStrategy()
+        with strategy.scope():
+            model = self.define_model("VGG11")
+            opt = Adam(lr=self.config.model.lr)
+            model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
         return model
