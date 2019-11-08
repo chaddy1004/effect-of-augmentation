499b3ecace5092c89d0712a7a10c1aa384c60ca1
Chad Paik <chad.paik@voyagerx.com>
2019-11-08 11:31:27 -0500

    removed multigpu code

--- network.py
+++ network.py
@@ -1,7 +1,6 @@
 from tensorflow.keras.layers import Input, Dense, Flatten, MaxPooling2D, LeakyReLU, Activation
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-import tensorflow as tf
 
 from layers import conv_block_2d, apply_padding
 
@@ -35,9 +34,7 @@ class VGG11():
         return Model(img, scores, name=model_name)
 
     def compile_model(self):
-        strategy = tf.distribute.MirroredStrategy()
-        with strategy.scope():
-            model = self.define_model("VGG11")
-            opt = Adam(lr=self.config.model.lr)
-            model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
+        model = self.define_model("VGG11")
+        opt = Adam(lr=self.config.model.lr)
+        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
         return model
